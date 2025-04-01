593b7bb82a49b09bfbb6d0f036a90592f7d70cbd
Cindy April Leochico <dakisleochico@gmail.com>
2025-04-01 17:24:15 -0600

    fix error App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 function App() {
-  return <h1> Name: Cindy Leochico</h1>;
-  <h1> Course: TECH2102:Enterprise Computing</h1>;
+  return (
+    <>
+      <h1>Name: Cindy Leochico</h1>
+      <h1>Course: TECH2102: Enterprise Computing</h1>
+    </>
+  );
 }
 
 export default App;
