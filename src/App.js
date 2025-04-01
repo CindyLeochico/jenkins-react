3e78c966b3c63e6fb196548741ef6a2d7be54273
Cindy April Leochico <dakisleochico@gmail.com>
2025-04-01 17:35:57 -0600

    Add course name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1>Name: Cindy Leochico</h1>
-      <h1>Course: TECH2102: Enterprise Computing</h1>
+      <h1>Course:Software Development TECH2102: Enterprise Computing</h1>
     </>
   );
 }
